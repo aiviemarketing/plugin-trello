c0ca7f845ca3c75a7cf532ac2e8520d81f52785b
Adrian Schimpf <adrian@aivie.ch>
2026-03-19 12:05:36 +0000

    chore(repo): enforce commit message style (initial version)

---
+++ commitlint.config.cjs
@@ -0,0 +1,48 @@
+/** @type {import('@commitlint/types').UserConfig} */
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "fix",
+        "feat",
+        "ux",
+        "obs",
+        "chore",
+        "perf",
+        "revert",
+        "docs",
+        "style",
+        "refactor",
+        "test",
+        "build",
+        "ci",
+        "security",
+      ],
+    ],
+    "scope-enum": [
+      2,
+      "always",
+      [
+        "trello",
+        "integration",
+        "contact",
+        "company",
+        "service",
+        "event",
+        "logging",
+        "security",
+        "repo",
+      ],
+    ],
+    "type-case": [2, "always", "lower-case"],
+    "type-empty": [2, "never"],
+    "scope-case": [2, "always", "lower-case"],
+    "scope-empty": [2, "never"],
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "header-max-length": [2, "always", 120],
+  },
+};
